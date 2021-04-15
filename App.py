515675824e829cf8470f946b3356c0bd43afa57c
akk0ga <45255672+akk0ga@users.noreply.github.com>
2021-04-16 01:03:02 +0200

    add jpg and png resized img

--- App.py
+++ App.py
@@ -3,10 +3,10 @@ from Image.Image import Image
 
 class App:
     def __init__(self):
-        self.image = Image('assets/square.jpg')
+        self.image = Image('assets/circle.png')
 
     def run(self):
-        self.image.show_image()
+        self.image.resize(new_width=20, new_height=20)
 
 
 if __name__ == "__main__":
--- Image/Image.py
+++ Image/Image.py
@@ -5,7 +5,11 @@ class Image:
     def __init__(self, image_path: str):
         self.image = Img.open(image_path)
 
-    def show_image(self):
+    def resize(self, new_width: int, new_height: int):
+        resize = self.image.resize((new_width, new_height))
+        resize.save(fp=f'assets/resized.{self.image.format}')
+
+    def show(self):
         """
         show the selected image
         :return:
