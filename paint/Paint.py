5fc5781396be449ca9e57fd4a6e0391e309a5820
akk0ga <45255672+akk0ga@users.noreply.github.com>
2021-04-16 01:56:34 +0200

    Update Paint.py

--- paint/Paint.py
+++ paint/Paint.py
@@ -2,4 +2,11 @@ from mouse.Mouse import Mouse
 
 
 class Paint:
-    pass
+    def __init__(self, mode: str = 'classic'):
+        """
+        mode is equal to\n
+        - classic (basic paint)
+        - 3d (if u use paint 3d)
+        :param mode:
+        """
+        self.mode = mode
